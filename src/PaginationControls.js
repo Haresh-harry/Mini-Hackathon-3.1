5bc440a39ed94b760a234782dd19b31fefa0d12d
haresh <138754420+Haresh-harry@users.noreply.github.com>
2025-04-02 12:30:18 +0530

    Update PaginationControls.js

--- src/PaginationControls.js
+++ src/PaginationControls.js
@@ -1,22 +1,24 @@
-const PaginationControls = ({ currentPage, totalPages, setCurrentPage }) => {
-    return (
-      <div className="inner">
-        <button onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))} disabled={currentPage === 1}>
-          Prev
+import React from "react";
+
+function PaginationControls ({ currentPage, totalPages, setCurrentPage }) {
+  return (
+    <div className="inner">
+      <button onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))} disabled={currentPage === 1}>
+        Prev
+      </button>
+
+      {[...Array(totalPages)].map((_, i) => (
+        <button key={i} onClick={() => setCurrentPage(i + 1)} style={{ fontWeight: currentPage === i + 1 ? "bold" : "normal" }}>
+          {i + 1}
         </button>
+      ))}
+
+      <button onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))} disabled={currentPage === totalPages}>
+        Next
+      </button>
+    </div>
+  );
+};
   
-        {[...Array(totalPages)].map((_, i) => (
-          <button key={i} onClick={() => setCurrentPage(i + 1)} style={{ fontWeight: currentPage === i + 1 ? "bold" : "normal" }}>
-            {i + 1}
-          </button>
-        ))}
-  
-        <button onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))} disabled={currentPage === totalPages}>
-          Next
-        </button>
-      </div>
-    );
-  };
-  
-  export default PaginationControls;
+export default PaginationControls;
   
